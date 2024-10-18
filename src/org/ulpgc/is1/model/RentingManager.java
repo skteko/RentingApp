bcead324c100393132f64952ab0b7776878687d7
jianxiao <jixiwa@gmail.com>
2024-10-18 23:56:32 +0100

    Customer related method done

--- src/org/ulpgc/is1/model/RentingManager.java
+++ src/org/ulpgc/is1/model/RentingManager.java
@@ -1,4 +1,20 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentingManager {
+    private List<Customer> CustomerList;
+    private List<Room> RoomList ;
+    public RentingManager() {
+        CustomerList = new ArrayList<>();
+        RoomList = new ArrayList<>() ;
+    }
+    public void addCustomer(String name, String surname, CustomerType type) {
+        CustomerList.add(new Customer(name, surname, type));
+    }
+    public List<Customer> getCustomer() {
+        return CustomerList;
+    }
+
 }
